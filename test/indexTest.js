277591a466c146bfe0b9626289c9926c060e2f4e
dahirfarhia44 <dahirfarhia44@gmail.com>
2026-02-21 21:35:59 +0300

    done

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,5 +1,7 @@
 require ( './helpers.js' );
 
+//let cats = ['Milo', 'Otis', 'Garfield'];
+
 describe('index.js', function () {
   describe('cats', function () {
     it('is assigned an initial value of ["Milo", "Otis", "Garfield"]', function () {
